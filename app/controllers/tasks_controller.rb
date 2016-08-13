3e112282c7ede3205238b83c02a8b848a8c1bc51
Ito Satoshi <satoshi-ito@outlook.jp>
2016-08-14 03:02:30 +0900

    夏休み課題

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,88 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  # GET /tasks
+  # GET /tasks.json
+  def index
+    @tasks = Task.all
+  end
+
+  # GET /tasks/1
+  # GET /tasks/1.json
+  def show
+  end
+
+  # GET /tasks/new
+  def new
+    @task = Task.new
+  end
+
+  # GET /tasks/1/edit
+  def edit
+  end
+
+  # POST /tasks
+  # POST /tasks.json
+  def create
+    @task = Task.new(task_params)
+
+    respond_to do |format|
+      if @task.save
+        format.html { redirect_to @task, notice: '登録完了' }
+        format.json { render :show, status: :created, location: @task }
+      else
+        format.html { render :new }
+        format.json { render json: @task.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /tasks/1
+  # PATCH/PUT /tasks/1.json
+  def update
+    respond_to do |format|
+      if @task.update(task_params)
+        format.html { redirect_to @task, notice: '更新完了' }
+        format.json { render :show, status: :ok, location: @task }
+      else
+        format.html { render :edit }
+        format.json { render json: @task.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /tasks/1
+  # DELETE /tasks/1.json
+  def destroy
+    @task.destroy
+    respond_to do |format|
+      format.html { redirect_to tasks_url, notice: '削除完了' }
+      format.json { head :no_content }
+    end
+  end
+
+  def search
+    @search_value = params['search']['name']
+    @search_user = params['search']['user']
+    @search_category = params['search']['category']
+    @tasks = Task.where("name like '%#{@search_value}%'")
+    if @search_user.present?
+      @tasks = @tasks.where(user_id: @search_user)
+    end
+    if @search_category.present?
+      @tasks = @tasks.where(category_id: @search_category)
+    end
+    render :index
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_task
+      @task = Task.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def task_params
+      params.require(:task).permit(:due_date, :category_id, :name, :done, :user_id)
+    end
+end
